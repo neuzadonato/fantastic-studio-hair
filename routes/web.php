9d5f820df314529020f95fbaf2a949cd6cdee19a
Raissa Heideriki <raissaleal535@gmail.com>
2023-03-29 21:38:22 -0300

    Adicionando template fantastic studio hair

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/faca-parte', function () {
+    return view('saloes.create');
+})->name('saloes.create');
