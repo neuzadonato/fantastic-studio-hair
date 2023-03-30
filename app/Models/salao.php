426439f3cb0d2b05b6867aa97a4bcaf5ceb38125
Neuza Donato <neuza.donato@gmail.com>
2023-03-29 22:28:27 -0300

    atualizacoes2903

---
+++ app/Models/salao.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Salao extends Model
+{
+    use HasFactory;
+
+    // Indica o nome da tabela do banco de dados
+    protected $table = "saloes";
+
+    // Indica os campos que podem ser preenchidos pelo usuário
+    protected $fillable = [
+        'id',
+        'nome',        
+        'endereco',
+        'telefone',
+        'salaografia',
+        'foto',
+    ];
+}
---
+++ database/migrations/2023_03_30_003244_create_saloes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('saloes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('razao_social');
+            $table->integer('endereco');
+            $table->string('telefone');
+            $table->text('salaografia');
+            $table->string('foto')->nullable()->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('saloes');
+    }
+};
