b97210b039f207fa1934825a015c759c4015f5d8
neuza.donato@gmail <neuza.donato@gmail.com>
2023-03-31 22:01:36 -0300

    alteracoes saloes controller php web php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SaloesController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,7 +19,28 @@ Route::get('/', function () {
     return view('home');
 });
 
+
 Route::get('/faca-parte', function () {
     return view('saloes.create');
 })->name('saloes.create');
+
+Route::get('/saloes', [SaloesController::class, 'index'])->name('saloes.index');
+
+// Rota para o formulário de criação de Saloes
+Route::get('/saloes/novo', [SaloesController::class, 'create'])->name('saloes.create');
+
+// Rota para o mostrar um gato específico
+Route::get('/salao/{salao}', [SaloesController::class, 'show'])->name('saloes.show');
+
+// Rota para o formulário de edição de saloes
+Route::get('/saloes/{salao}/editar', [SaloesController::class, 'edit'])->name('saloes.edit');
+
+// Rota que armazena um novo gato no banco de dados
+Route::post('/saloes', [SaloesController::class, 'store'])->name('saloes.store');
+
+// Rota que atualiza um gato específico no banco de dados
+Route::put('/Saloes/{gato}', [SaloesController::class, 'update'])->name('saloes.update');
+
+// Rota que remove um gato específico do banco de dados
+Route::delete('/Saloes/{gato}', [SaloesController::class, 'destroy'])->name('saloes.destroy');
 
