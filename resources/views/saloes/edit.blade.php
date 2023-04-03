feee534a8549cf80c624f86cebc1cebd79df39c6
amanda landucci <heloisapt_gpsbr@hotmail.com>
2023-04-03 20:46:58 -0300

    Edit blade

--- resources/views/saloes/edit.blade.php
+++ resources/views/saloes/edit.blade.php
@@ -6,43 +6,43 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-        <title>Usuario #{{ $usuario->id }}</title>
+        <title>Salao #{{ $salao->id }}</title>
     </head>
     <body>
         <div class="container">
-            <h1>Edição do usuario {{ $usuario->id }}</h1>
+            <h1>Edição do salão {{ $salao->id }}</h1>
 
-            <form method="POST" action="{{ route('usuarios.update', $usuario->id) }}">
+            <form method="POST" action="{{ route('saloes.update', $salao->id) }}">
                 @csrf
                 @method('PUT')
 
                 <div class="mb-3">
                     <label>Nome</label>
-                    <input type="text" name="nome" class="form-control" value="{{ $usuario->nome }}">
+                    <input type="text" name="nome" class="form-control" value="{{ $salao->nome }}">
                 </div>
 
                 <div class="mb-3">
                     <label>Telefone</label>
-                    <input type="text" name="telefone" class="form-control" value="{{ $usuario->telefone }}">
+                    <input type="text" name="telefone" class="form-control" value="{{ $salao->telefone }}">
                 </div>
 
                 <div class="mb-3">
                     <label>Email</label>
-                    <input type="number" name="email" class="form-control" value="{{ $usuario->email }}">
+                    <input type="number" name="email" class="form-control" value="{{ $salao->email }}">
                 </div>
 
                 <div class="mb-3">
                     <label>Endereço</label>
-                    <input type="text" name="endereco" class="form-control" value="{{ $usuario->endereco }}">
+                    <input type="text" name="endereco" class="form-control" value="{{ $salao->endereco }}">
                 </div>
 
                 <div class="mb-3">
                     <label>Salão</label>
-                    <textarea name="salao" id="" rows="5" class="form-control">{{ $usuario->salao }}</textarea>
+                    <textarea name="salao" id="" rows="5" class="form-control">{{ $salao->salao }}</textarea>
                 </div>
 
                 <div>
-                    <input type="submit" value="Atualizar Usuario" class="btn btn-primary">
+                    <input type="submit" value="Atualizar Salao" class="btn btn-primary">
                 </div>
             </form>
         </div>
