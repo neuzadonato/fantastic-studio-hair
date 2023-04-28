813eaecbd90c730012f096d867113c438d87f416
neuzadonato <neuza.donato@gmail.com>
2023-04-28 20:48:46 -0300

    Alterações

--- resources/views/saloes/create.blade.php
+++ resources/views/saloes/create.blade.php
@@ -8,7 +8,9 @@
             <div class="row">
 
                 <div class="col-lg-6 col-10 mx-auto">
-                    <form class="custom-form ticket-form mb-5 mb-lg-0" action="#" method="post" role="form">
+                    <form class="custom-form ticket-form mb-5 mb-lg-0" action="{{ route('saloes.store') }}" method="post" role="form">
+                        @csrf
+
                         <h2 class="text-center mb-4">Cadastro</h2>
 
                         <script>
@@ -38,8 +40,12 @@
                         <div class="ticket-form-body">
                             <div class="row">
                                 <div class="col-lg-15 col-md-15 col-12">
-                                    <input type="text" name="name" id="name" class="form-control"
+                                    <input type="text" name="nome" id="name" class="form-control"
                                         placeholder="Nome" required>
+
+                                    @error('nome')
+                                        <div class="alert alert-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
 
                                 <div class="col-lg-15 col-md-15 col-12">
@@ -71,16 +77,6 @@
                             <div class="col-lg-15 col-md-15 col-12">
                                 <input type="image" name="foto" id="foto"
                                     class="form-control" placeholder="Foto" required>
-                            </div>
-
-                            <div class="col-lg-15 col-md-15 col-12">
-                                <input type="password" name="senha" id="senha" class="form-control"
-                                    placeholder="Senha" required>
-                            </div>
-
-                            <div class="col-lg-15 col-md-15 col-12">
-                                <input type="password" name="confirmarsenha" id="ConfirmarSenha" class="form-control"
-                                    placeholder="Confirmar Senha" required>
                             </div>
 
                             <div class="col-lg-4 col-md-10 col-8 mx-auto">
