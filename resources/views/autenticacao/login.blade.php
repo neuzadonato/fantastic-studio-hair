038c5422fe3a67d0dcb64e9c8cfd4351611dee73
raissaheideriki <raissaleal535@gmail.com>
2023-04-28 22:49:17 -0300

    login

--- resources/views/autenticacao/login.blade.php
+++ resources/views/autenticacao/login.blade.php
@@ -5,7 +5,28 @@
         <div class="section-overlay"></div>
 
         <div class="container">
+            <form class="custom-form contact-form mb-5 mb-lg-0" action="#" method="post"
+            role="form">
+            <div class="contact-form-body">
+                <div class="row">
 
+
+<div class="col-lg-6 col-md-6 col-12">
+                        <input type="usuario" name="usuario"  class="form-control"  placeholder="Usuário" value="<?php if(isset($dados['usuario'])){ echo $dados['usuario']; } ?>">
+
+                    </div>
+<div class="col-lg-6 col-md-6 col-12">
+                            <input type="password" name="senha_usuario"  class="form-control"  placeholder="Senha" value="<?php if(isset($dados['senha_usuario'])){ echo $dados['senha_usuario']; } ?>"> </div>
+
+</div>
+
+
+
+
+
+
+                    <button type="submit" class="form-control"  value="Acessar" name="SendLogin"  >Logar    </button>
+                </div>
         </div>
     </section>
 @endsection
