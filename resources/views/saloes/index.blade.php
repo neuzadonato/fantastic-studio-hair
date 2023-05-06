6261c98a792ef6aba3083a8a850984dbfcee0470
neuzadonato <neuza.donato@gmail.com>
2023-05-05 21:35:17 -0300

    alteracao da duplicacao

--- resources/views/saloes/index.blade.php
+++ resources/views/saloes/index.blade.php
@@ -1,8 +1,3 @@
-
-
-
-
-
         @extends('templates.layout')
 
         @section('conteudo')
@@ -10,12 +5,6 @@
                 <div class="section-overlay"></div>
 
                 <div class="container">
-
-
-                    <title>Lista de Saloes</title>
-                </head>
-                <body>
-                    <div class="container">
                         <h1>Lista de Saloes</h1>
 
                         <a class="btn btn-outline-success my-2" href="{{ route('saloes.create') }}">Novo Salao</a>
@@ -55,73 +44,7 @@
                             @endforeach
                         </table>
                     </div>
-
-                </body>
-            </html>
-
-
-
-
-                </div>
             </section>
         @endsection
 
-
-
-
-
-
-
-
-
-
-
-
-        <title>Lista de Saloes</title>
-    </head>
-    <body>
-        <div class="container">
-            <h1>Lista de Saloes</h1>
-
-            <a class="btn btn-outline-success my-2" href="{{ route('saloes.create') }}">Novo Salao</a>
-
-            <table class="table table-hover table-bordered table-primary">
-                <tr class="table-dark">
-                    <th>nome</th>
-                    <th>razao_social</th>
-                    <th>email</th>
-                    <th>endereco</th>
-                    <th>telefone</th>
-                    <th>salaografia</th>
-                    <th>foto</th>
-                    <th>senha</th>
-                    <th>confirmar_senha</th>
-                    <th></th>
-                </tr>
-
-                @foreach ($saloes as $salao)
-                    <tr>
-                        <td>{{ $salao->nome }}</td>
-                        <td>{{ $salao->razao_social }}</td>
-                        <td>{{ $salao->email }}</td>
-                        <td>{{ $salao->endereco }}</td>
-                        <td>{{ $salao->telefone }}</td>
-                        <td>{{ $salao->salaogragfia }}</td>
-                        <td>{{ $salao->foto }}</td>
-                        <td>{{ $salao->senha }}</td>
-                        <td>{{ $salao->confirmar_senha }}</td>
-
-                        <td>
-                            <a class="link" href="{{ route('saloes.show', $salao->id) }}">
-                                Ver
-                            </a>
-                        </td>
-                    </tr>
-                @endforeach
-            </table>
-        </div>
-
-
-    </body>
-</html>
 
