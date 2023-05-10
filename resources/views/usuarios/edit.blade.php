c0ed98f7bccac30b78a330fdd3f12402e2e96e7a
raissaheideriki <raissaleal535@gmail.com>
2023-05-09 22:29:58 -0300

    09-05

--- resources/views/usuarios/edit.blade.php
+++ resources/views/usuarios/edit.blade.php
@@ -12,27 +12,27 @@
                 @method('PUT')
 
                 <div class="mb-3">
-                    <label>Nome</label>
+                    <label class="text-white">Nome: </label>
                     <input type="text" name="nome" class="form-control" value="{{ $usuario->nome }}">
                 </div>
 
                 <div class="mb-3">
-                    <label>Telefone</label>
+                    <label class="text-white">Telefone: </label>
                     <input type="text" name="telefone" class="form-control" value="{{ $usuario->telefone }}">
                 </div>
 
                 <div class="mb-3">
-                    <label>Email</label>
+                    <label class="text-white">Email: </label>
                     <input type="number" name="email" class="form-control" value="{{ $usuario->email }}">
                 </div>
 
                 <div class="mb-3">
-                    <label>Endereço</label>
+                    <label class="text-white">Endereço: </label>
                     <input type="text" name="endereco" class="form-control" value="{{ $usuario->endereco }}">
                 </div>
 
                 <div class="mb-3">
-                    <label>Salão</label>
+                    <label class="text-white">Salãografia: </label>
                     <textarea name="salao" id="" rows="5" class="form-control">{{ $usuario->salao }}</textarea>
                 </div>
 
