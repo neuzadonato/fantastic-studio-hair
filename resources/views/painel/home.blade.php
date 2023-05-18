6bd6ef2690be60c73ccf84defaa7b89bae73554f
neuzadonato <neuza.donato@gmail.com>
2023-05-17 21:58:57 -0300

    alteracao

--- resources/views/painel/home.blade.php
+++ resources/views/painel/home.blade.php
@@ -7,6 +7,17 @@
         <div class="container">
             <h1 class="text-center text-white "> PAINEL DO ADMINISTRADOR!</h1>
 
+            <style>
+                .container1 {
+                    max-width: 250px;
+                    margin: 0 auto;
+                    padding: 20px;
+                    background-color: #EE5007;
+                    border-radius: 25px;
+                    margin-top: 100px;
+                }
+            </style>
+                <div class="container1">
             <nav>
                 <a href="{{ route('saloes.create') }}">Cadastrar  Salão</a>
 
