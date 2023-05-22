056058f62b37149eefb5cb58e6982b3b590153c3
stefani <stheffanibrandao@gmail.comn>
2023-05-22 20:35:26 -0300

    tamanho dos blocos do salão

--- resources/views/templates/saloes.blade.php
+++ resources/views/templates/saloes.blade.php
@@ -6,7 +6,7 @@
 
                 <div class="row g-3 py-3 justify-content-center">
                     @foreach($saloes as $salao)
-                        <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+                        <div class="col-12 col-sm-12 col-md-10 col-lg-6 col-xl-15">
                             <a href="{{ route('site.salao', [$salao->nome, $salao->id]) }}">
                                 <div class="card p-4 border bg-white">
                                     <h1>{{ $salao->nome }}</h1>
