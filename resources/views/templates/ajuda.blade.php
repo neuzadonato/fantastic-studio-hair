f90af2fb86d85f0e34d8546d1c916f95e78ed95b
stefani <stheffanibrandao@gmail.comn>
2023-05-22 21:45:06 -0300

    alteração do texto

--- resources/views/templates/ajuda.blade.php
+++ resources/views/templates/ajuda.blade.php
@@ -5,14 +5,15 @@
             <div class="col-lg-8 col-12 mx-auto">
                 <h2 class="text-center mb-4">Precisa de ajuda ?</h2>
             </div>
-            
+
             <div class="col-lg-26 col-12 mt-4 mt-lg-0">
                 <div class="pricing-thumb">
                     <div class="d-flex">
                         <div>
                             <h3 class= "text-center mb-4"> Como fazer seu LINK de encaminhamento para o WhatsApp </h3>
 
-                            <p>Link para o WhatsApp é uma funcionalidade do aplicativo que permite iniciar uma conversa com apenas um clique. Para criar um, basta digitar https://wa.me/<seunúmero> </p>
+                            <p class= "text-center mb-4">Link para o WhatsApp é uma funcionalidade do aplicativo que permite iniciar uma
+                            conversa com apenas um clique. Para criar um, basta digitar https://wa.me/"seunúmero" </p>
                         </div>
                     </div>
                 </div>
