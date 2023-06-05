34cf7a5797d1acef3eb386eac03ec1222a56ac59
neuzadonato <neuza.donato@gmail.com>
2023-06-05 20:45:32 -0300

    alteracao cixa salao e servicos

--- resources/views/servicos/index.blade.php
+++ resources/views/servicos/index.blade.php
@@ -1,7 +1,7 @@
 @extends('templates.layout')
 
         @section('conteudo')
-            <section class="ticket-section section-padding">
+        <section class="about-section section-padding">
                 <div class="section-overlay"></div>
 
                 <div class="container">
