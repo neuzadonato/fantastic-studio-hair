01045df6af1ee57dc2e4d8b9cb68670c0ac2b0b7
neuzadonato <neuza.donato@gmail.com>
2023-06-06 19:57:13 -0300

    alteracao

--- resources/views/avaliacoes/edit.blade.php
+++ resources/views/avaliacoes/edit.blade.php
@@ -6,34 +6,34 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-        <title>Avaliação #{{ $avaliacao->id }}</title>
+        <title>Avaliação {{ $Avaliacao->id }}</title>
     </head>
     <body>
         <div class="container">
-            <h1>Edição da avaliação {{ $avaliacao->id }}</h1>
+            <h1>Edição da avaliação {{ $Avaliacao->id }}</h1>
 
-            <form method="POST" action="{{ route('avaliacoes.update', $avaliacao->id) }}">
+            <form method="POST" action="{{ route('avaliacoes.update', $Avaliacao->id) }}">
                 @csrf
                 @method('PUT')
 
                 <div class="mb-3">
                     <label>Nome</label>
-                    <input type="text" name="nome" class="form-control" value="{{ $avaliacao->nome }}">
+                    <input type="text" name="nome" class="form-control" value="{{ $Avaliacao->nome }}">
                 </div>
 
                 <div class="mb-3">
                     <label>Email</label>
-                    <input type="text" name="email" class="form-control" value="{{ $avaliacao->email }}">
+                    <input type="text" name="email" class="form-control" value="{{ $Avaliacao->email }}">
                 </div>
 
                 <div class="mb-3">
                     <label>Nota</label>
-                    <input type="number" name="nota" class="form-control" value="{{ $avaliacao->nota }}">
+                    <input type="number" name="nota" class="form-control" value="{{ $Avaliacao->nota }}">
                 </div>
 
                 <div class="mb-3">
                     <label>Mensagem</label>
-                    <input type="number" name="mensagem" class="form-control" value="{{ $avaliacao->mensagem }}">
+                    <input type="text" name="mensagem" class="form-control" value="{{ $Avaliacao->mensagem }}">
                 </div>
 
                 <div>
